76051c41fe06c7ce9708f6d2b517d53733622eaa
soutchay <chungvus@gmail.com>
2015-04-21 20:17:56 -0700

    euler 2 complete

--- two.go
+++ two.go
@@ -8,22 +8,35 @@ import (
 
 func main() {
   fmt.Printf("Euler #2\n")
-  fibonacci(5)
+  fibonacci(100)
+  evenSum(fibonacci(50))
 }
 
-func fibonacci(n int) {
+func fibonacci(n int) (fib map[int]int){
   fibMap := map[int]int{}
-  // fmt.Println(fibMap[0])
   for i:=0; i<n+1; i++ {
     if i == 0 {
       fibMap[0] = 1
     } else if i == 1{
       fibMap[1] = 2
     }
-    if fibMap[i] == 0 {
+    if fibMap[i] == 0 { //if doesn't exist in fibMap
       fibMap[i] = fibMap[i-1] + fibMap[i-2]
     }
-
   }
-  fmt.Println(fibMap)
+  fmt.Println("Fibonacci number", n, ":", fibMap[n])
+  return fibMap
+}
+
+func evenSum(a map[int]int) {
+  fibSlice := []int{} //slice of the numbers that are less than 4million and even
+  fibSum := 0
+  for k, _ := range a {
+    if a[k] < 4000000 && a[k]%2 == 0{
+      fibSlice = append(fibSlice, a[k])
+      fibSum += a[k]
+    }
+  }
+  fmt.Println("The sum of even and less than 4million: ", fibSum)
+
 }
