4384e389d4a659577ba50f35fe2337b47b157305
soutchay <chungvus@gmail.com>
2015-05-10 22:34:32 -0700

    a solution for euler 6

---
+++ six.go
@@ -0,0 +1,57 @@
+/* Euler 6
+The sum of the squares of the first ten natural numbers is,
+
+1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is,
+
+(1 + 2 + ... + 10)^2 = 552 = 3025
+Hence the difference between the sum of the squares of the first ten natural numbers and
+the square of the sum is 3025 − 385 = 2640.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers
+and the square of the sum.
+*/
+
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("Euler 6")
+  start := time.Now()
+  fmt.Println(differenceBrute(sumSquares(100), squareSum(100)))
+  fmt.Println("It took brute:", time.Since(start))
+}
+
+func sumSquares(a int) int{
+  sum := 0
+  for i:=1;i<=a; i++{
+    sum += power(i, 2)
+  }
+  return sum
+}
+
+func squareSum(a int) int{
+  sum := 0
+  for i:=1; i<=a; i++ {
+    sum += i
+  }
+  return power(sum, 2)
+}
+
+func differenceBrute(a int, b int) int {
+  //a is sum of the squares, b is square of the sum
+  return b-a
+}
+
+func power(a int, b int) int{
+  //a is the base, b is the exponent
+  value := 1
+  for i:=0; i<b; i++ {
+    value *= a
+  }
+  return value
+}
